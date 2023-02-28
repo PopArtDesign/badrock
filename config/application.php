325b5445395ab0b3ab1d09fde59eb903975e1b3d
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2023-02-28 18:49:41 +0300

    Set FS_METHOD to "direct" by default

--- config/application.php
+++ config/application.php
@@ -114,6 +114,7 @@ Config::define('DISALLOW_FILE_EDIT', true);
 Config::define('DISALLOW_FILE_MODS', true);
 // Limit the number of post revisions that Wordpress stores (true (default WP): store every revision)
 Config::define('WP_POST_REVISIONS', env('WP_POST_REVISIONS') ?? 5);
+Config::define('FS_METHOD', 'direct');
 
 /**
  * Debugging Settings
