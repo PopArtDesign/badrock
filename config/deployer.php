b8a0ea4e7d9631819d67b6011f029c1b5d41b963
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2023-03-05 04:50:41 +0300

    Setup languages and migrate db during deployment

--- config/deployer.php
+++ config/deployer.php
@@ -20,29 +20,19 @@ add('writable_dirs', [
 set('build_dir', dirname(__DIR__) . '/var/build');
 
 // Tasks
-desc('Prepare build directory');
-task('badrock:build:prepare', function () {
+desc('Checkout repo');
+task('badrock:checkout', function () {
     runLocally('rm -rf "{{build_dir}}"');
     runLocally('mkdir -p "{{build_dir}}"');
-});
-
-desc('Checkout repo');
-task('badrock:build:checkout', function () {
     runLocally('git --work-tree="{{build_dir}}" checkout -f {{target}}');
 });
 
-desc('Install Phive tools');
-task('badrock:build:tools', function () {
+desc('Install tools (wp-cli)');
+task('badrock:tools', function () {
     runLocally('phive install --copy wp', [
         'cwd' => get('build_dir'),
     ]);
 });
-
-task('badrock:build', [
-    'badrock:build:prepare',
-    'badrock:build:checkout',
-    'badrock:build:tools',
-]);
 
 task('deploy:update_code', function () {
     set('rsync_src', '{{build_dir}}');
@@ -50,10 +40,61 @@ task('deploy:update_code', function () {
     invoke('rsync');
 });
 
+desc('Install WordPress languages');
+task('badrock:languages', function () {
+    $languages = get('languages');
+
+    if (empty($languages)) {
+        return;
+    }
+
+    if (is_array($languages)) {
+        $languages = implode(' ', $languages);
+    }
+
+    cd('{{release_or_current_path}}');
+
+    if (!test('tools/wp core is-installed')) {
+        return;
+    }
+
+    run('tools/wp language core install ' . $languages);
+    run('tools/wp language plugin install --all '. $languages);
+    run('tools/wp language theme install --all ' . $languages);
+    run('tools/wp language core update');
+    run('tools/wp language plugin update --all');
+    run('tools/wp language theme update --all');
+});
+
+task('badrock:db-migrations', function () {
+    cd('{{release_or_current_path}}');
+
+    if (!test('tools/wp core is-installed')) {
+        return;
+    }
+
+    run('tools/wp core update-db');
+
+    if (!test('tools/wp plugin is-installed woocommerce')) {
+        run('tools/wp wc update');
+    }
+});
+
+task('badrock:build', [
+    'badrock:checkout',
+    'badrock:tools',
+]);
+
+task('badrock:deploy', [
+    'badrock:languages',
+    'badrock:db-migrations',
+]);
+
 task('deploy', [
     'badrock:build',
     'deploy:prepare',
     'deploy:vendors',
+    'badrock:deploy',
     'deploy:publish',
 ]);
 
