5122f4955440c6e04ac867d6c541184af8a34a1b
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2023-03-05 03:46:31 +0300

    Add simple recipe for Deployer

---
+++ config/deployer.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/common.php';
+require 'contrib/rsync.php';
+
+// Config
+add('shared_files', []);
+
+add('shared_dirs', [
+    'public/uploads',
+    'var/log',
+]);
+
+add('writable_dirs', [
+    'public/uploads'
+]);
+
+set('build_dir', realpath(__DIR__ . '/../var/build'));
+
+// Tasks
+desc('Prepare build directory');
+task('badrock:build:prepare', function () {
+    runLocally('rm -rf "{{build_dir}}"');
+    runLocally('mkdir -p "{{build_dir}}"');
+});
+
+desc('Checkout repo');
+task('badrock:build:checkout', function () {
+    runLocally('git --work-tree="{{build_dir}}" checkout -f {{target}}');
+});
+
+desc('Install Phive tools');
+task('badrock:build:tools', function () {
+    runLocally('phive install --copy wp', [
+        'cwd' => get('build_dir'),
+    ]);
+});
+
+task('badrock:build', [
+    'badrock:build:prepare',
+    'badrock:build:checkout',
+    'badrock:build:tools',
+]);
+
+task('deploy:update_code', function () {
+    set('rsync_src', '{{build_dir}}');
+
+    invoke('rsync');
+});
+
+task('deploy', [
+    'badrock:build',
+    'deploy:prepare',
+    'deploy:vendors',
+    'deploy:publish',
+]);
+
+// Hooks
+after('deploy:failed', 'deploy:unlock');
