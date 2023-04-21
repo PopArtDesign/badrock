a666c091663b05b8a504fd3f6119ec3b1c6ba982
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2023-04-22 01:25:56 +0300

    Disable Soil modules: "js-to-footer" and "nav-walker"

--- wp-config.php
+++ wp-config.php
@@ -156,8 +156,6 @@ Config::define('SOIL', [
     'clean-up',
     'disable-asset-versioning',
     'disable-trackbacks',
-    'js-to-footer',
-    'nav-walker',
     'nice-search',
     'relative-urls',
 ]);
